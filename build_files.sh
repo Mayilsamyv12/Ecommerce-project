c0788dd8c83b7dcb07e0a3d774a270f06204a407
Mayilsamyv12 <mayilsamyv12@gmail.com>
2026-02-07 11:53:15 +0530

    Fixed build script (PEP 668) and requirements encoding

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-pip install -r requirements.txt
+# Create and activate virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Upgrade pip and install requirements
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+
+# Run Django commands
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
